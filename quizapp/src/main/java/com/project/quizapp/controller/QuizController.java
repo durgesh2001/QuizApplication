888d0b7086189c1a9be1db16c8eae934feff075f
durgesh <michaeldurgesh@gmail.com>
2025-05-31 19:05:57 +0530

    Quiz by Random number of questions functionality created

--- quizapp/src/main/java/com/project/quizapp/controller/QuizController.java
+++ quizapp/src/main/java/com/project/quizapp/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.project.quizapp.controller;
 
 
+import com.project.quizapp.model.QuestionWrapper;
+import com.project.quizapp.model.Response;
 import com.project.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,5 +20,16 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category,numQ,title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>>  getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses ){
+        return quizService.calculateResult(id, responses);
+
     }
 }
