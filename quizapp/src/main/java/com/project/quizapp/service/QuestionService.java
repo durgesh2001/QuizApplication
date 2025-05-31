312b682c9c3ce5820607fc80f5437133dfa22bfe
durgesh <michaeldurgesh@gmail.com>
2025-05-31 17:32:40 +0530

    Questions table has been implemented

---
+++ quizapp/src/main/java/com/project/quizapp/service/QuestionService.java
@@ -0,0 +1,56 @@
+package com.project.quizapp.service;
+
+import com.project.quizapp.model.Question;
+import com.project.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questionDao.save(question);
+            return new ResponseEntity<>("success", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Unsucess", HttpStatus.NOT_MODIFIED);
+    }
+
+    public ResponseEntity<String> deleteQuestion(Integer id) {
+        try{
+            questionDao.deleteById(id);
+            return new ResponseEntity<>("deleted", HttpStatus.OK);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Not deleted", HttpStatus.NOT_MODIFIED);
+    }
+}
